4c0023aafb507d540bec728b60d5ef4969aaf815
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2023-12-23 01:32:32 -0500

    chan

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import Header from './components/header/Header';
 import Profile from './components/profile/Profile';
 import About from './components/about/About';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as  Routes, Route } from 'react-router-dom';
 import Contact from './components/contact/Contact';
 import Skills from './components/skills/Skills';
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <div className="App">
           <Header />
           <Routes>
-            <Route path="/" element={<Profile />} />
+            <Route path="https://sudhanva1999.github.io/profile-portfolio/" element={<Profile />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Header />} />
             <Route path="/skills" element={<Skills />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
