f5d9f9e715da470588b1922ab61b13fa4b77ddbc
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2023-12-28 11:22:31 -0500

    project changes.

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -24,6 +24,22 @@ import javaimg4 from '../../resources/projectimages/1_java/4_java.png'
 import javaimg5 from '../../resources/projectimages/1_java/5_java.png'
 import javaimg6 from '../../resources/projectimages/1_java/6_java.png'
 
+import handimg1 from '../../resources/projectimages/3_hand/hand_1.png'
+import handimg2 from '../../resources/projectimages/3_hand/hand_2.png'
+import handimg3 from '../../resources/projectimages/3_hand/hand_3.png'
+import handimg4 from '../../resources/projectimages/3_hand/hand_4.png'
+import handimg5 from '../../resources/projectimages/3_hand/hand_5.png'
+
+import portalimg1 from '../../resources/projectimages/4_portal/portal_1.png'
+import portalimg2 from '../../resources/projectimages/4_portal/portal_2.png'
+import portalimg3 from '../../resources/projectimages/4_portal/portal_3.png'
+
+
+import fakeNewsImg1 from '../../resources/projectimages/5_fakeNews/fakenews_1.png'
+import fakeNewsImg2 from '../../resources/projectimages/5_fakeNews/fakenews_2.png'
+import fakeNewsImg3 from '../../resources/projectimages/5_fakeNews/fakenews_3.png'
+
+
 
 
 
@@ -37,6 +53,27 @@ const slides1 = [
     <img className='project-slides' key={4} src={javaimg4} alt="Slide 4" />,
     <img className='project-slides' key={5} src={javaimg5} alt="Slide 5" />,
     <img className='project-slides' key={6} src={javaimg6} alt="Slide 6" />,
+  ];
+
+  const slidesHand = [
+    <img className='project-slides' key={3} src={handimg1} alt="Slide 3" />,
+    <img className='project-slides' key={1} src={handimg2} alt="Slide 1" />,
+    <img className='project-slides' key={2} src={handimg3} alt="Slide 2" />,
+    <img className='project-slides' key={4} src={handimg4} alt="Slide 4" />,
+    <img className='project-slides' key={5} src={handimg5} alt="Slide 5" />,
+  ];
+
+
+  const portalimgs = [
+    <img className='project-slides' key={3} src={portalimg1} alt="Slide 3" />,
+    <img className='project-slides' key={1} src={portalimg2} alt="Slide 1" />,
+    <img className='project-slides' key={2} src={portalimg3} alt="Slide 2" />,
+  ];
+
+  const fakeNewsImg = [
+    <img className='project-slides' key={3} src={fakeNewsImg1} alt="Slide 3" />,
+    <img className='project-slides' key={1} src={fakeNewsImg2} alt="Slide 1" />,
+    <img className='project-slides' key={2} src={fakeNewsImg3} alt="Slide 2" />,
   ];
 
 const Projects = () => {
@@ -54,8 +91,7 @@ const Projects = () => {
   return (
     <div className="projects-container">
       <h1>My Projects</h1>
-
-      {/* Rows of project names that trigger scrolling on click */}
+      <h3>Click for details !</h3><br/>
       <div className="project-list">
         <div className="project-name" onClick={() => scrollToProject('project1')}>
           Image Processing Software
@@ -76,7 +112,7 @@ const Projects = () => {
         
 
         </div>
-        <div className="project-name" onClick={() => scrollToProject('project2')}>
+        <div className="project-name" onClick={() => scrollToProject('project3')}>
         RNN Based Handwriting Classifier and Analysis Tool 
         <div>
         <img className="actual-icon" src={pythonicon} />
@@ -87,7 +123,7 @@ const Projects = () => {
         </div>
        
         </div>
-        <div className="project-name" onClick={() => scrollToProject('project2')}>
+        <div className="project-name" onClick={() => scrollToProject('project4')}>
         Paper Presentation Conference Portal	
         <div>
         <img className="actual-icon" src={phpicon} />
@@ -98,7 +134,7 @@ const Projects = () => {
         </div>
 	
         </div>
-        <div className="project-name" onClick={() => scrollToProject('project2')}>
+        <div className="project-name" onClick={() => scrollToProject('project5')}>
         CNN based Fake News Detector 	
         <div>
         <img className="actual-icon" src={pythonicon} />
@@ -107,7 +143,7 @@ const Projects = () => {
         <img className="actual-icon" src={booticon} />
         </div>	
         </div> 
-        <div className="project-name" onClick={() => scrollToProject('project2')}>
+        <div className="project-name" onClick={() => scrollToProject('project6')}>
         Client Host Multimedia Player	
         <div>
         <img className="actual-icon" src={pythonicon} />
@@ -116,7 +152,7 @@ const Projects = () => {
         </div>	
         </div>
 
-        <div className="project-name" onClick={() => scrollToProject('project2')}>
+        <div className="project-name">
         This Site ! 
         <div>
         <img className="actual-icon" src={reacticon} />
@@ -164,8 +200,6 @@ const Projects = () => {
                 </p>
             </div>
         </div>
-        
-      
       </div>
 
       <div id="project2" className="project-section">
@@ -187,8 +221,9 @@ const Projects = () => {
                     The Complex Team Management Tool is a sophisticated React application 
                     designed to streamline and enhance the collaborative workflow within 
                     complex team structures. Built with a focus on incorporating fundamental
-                     React concepts, this app serves as a showcase for various advanced 
-                     front-end development techniques.   
+                    React concepts, this app serves as a showcase for various advanced 
+                    front-end development techniques. The app also uses data from rest apis 
+                    and uses Google's Firestore to store user details. 
                 </p>
                 <br/>
                 <p>
@@ -204,11 +239,175 @@ const Projects = () => {
                 </p>
             </div>
         </div>
-        
-      
       </div>
-     
+
+      <div id="project3" className="project-section">
+        <h2>
+        RNN Based Handwriting Classifier and Analysis Tool 
+        <button className="back-to-top" onClick={scrollToTop}>
+          Back to Top
+        </button>
+        </h2> 
+        <div>
+        <Carousel slides={slidesHand} />
+            <div>
+                <br/>
+                <h3>
+                    <strong>
+                    What the project is about: 
+                    </strong>
+                    </h3>
+                <p>
+                    The Complex Team Management Tool is a sophisticated React application 
+                    designed to streamline and enhance the collaborative workflow within 
+                    complex team structures. Built with a focus on incorporating fundamental
+                    React concepts, this app serves as a showcase for various advanced 
+                    front-end development techniques. The app also uses data from rest apis 
+                    and uses Google's Firestore to store user details. 
+                </p>
+                <br/>
+                <p>
+                    <strong>Key Features:</strong> <br/>
+                   - Context API for Global State : The Context API is employed to manage global state across components, ensuring a centralized and accessible data source.
+                                                    This approach simplifies state management in larger applications with complex component hierarchies.<br/>
+                   - Hooks : React Hooks, such as useState and useEffect, are used to enhance the functional components with stateful logic.
+                             This modern approach allows functional components to manage state and side effects without the need for class components.<br/>
+                   - Optimizing Performance : React's performance optimization strategies, including memoization and PureComponent, are implemented to ensure efficient rendering.
+                                              This results in a responsive user interface with minimized unnecessary re-renders.<br/>
+                   - Conditional Rendering : React's conditional rendering capabilities are employed to display different views based on the application's state.
+                                             This ensures that users are presented with relevant information and features depending on the context of their interactions.
+                </p>
+            </div>
+        </div>
+      </div>
+
+      <div id="project4" className="project-section">
+        <h2>
+        Paper Presentation Conference Portal
+        <button className="back-to-top" onClick={scrollToTop}>
+          Back to Top
+        </button>
+        </h2> 
+        <div>
+        <Carousel slides={portalimgs} />
+            <div>
+                <br/>
+                <h3>
+                    <strong>
+                    What the project is about: 
+                    </strong>
+                    </h3>
+                <p>
+                    The Complex Team Management Tool is a sophisticated React application 
+                    designed to streamline and enhance the collaborative workflow within 
+                    complex team structures. Built with a focus on incorporating fundamental
+                    React concepts, this app serves as a showcase for various advanced 
+                    front-end development techniques. The app also uses data from rest apis 
+                    and uses Google's Firestore to store user details. 
+                </p>
+                <br/>
+                <p>
+                    <strong>Key Features:</strong> <br/>
+                   - Context API for Global State : The Context API is employed to manage global state across components, ensuring a centralized and accessible data source.
+                                                    This approach simplifies state management in larger applications with complex component hierarchies.<br/>
+                   - Hooks : React Hooks, such as useState and useEffect, are used to enhance the functional components with stateful logic.
+                             This modern approach allows functional components to manage state and side effects without the need for class components.<br/>
+                   - Optimizing Performance : React's performance optimization strategies, including memoization and PureComponent, are implemented to ensure efficient rendering.
+                                              This results in a responsive user interface with minimized unnecessary re-renders.<br/>
+                   - Conditional Rendering : React's conditional rendering capabilities are employed to display different views based on the application's state.
+                                             This ensures that users are presented with relevant information and features depending on the context of their interactions.
+                </p>
+            </div>
+        </div>
+      </div>
+
+      <div id="project5" className="project-section">
+        <h2>
+        CNN based Fake News Detector 	
+        <button className="back-to-top" onClick={scrollToTop}>
+          Back to Top
+        </button>
+        </h2> 
+        <div>
+        <Carousel slides={fakeNewsImg} />
+            <div>
+                <br/>
+                <h3>
+                    <strong>
+                    What the project is about: 
+                    </strong>
+                    </h3>
+                <p>
+                    The Complex Team Management Tool is a sophisticated React application 
+                    designed to streamline and enhance the collaborative workflow within 
+                    complex team structures. Built with a focus on incorporating fundamental
+                    React concepts, this app serves as a showcase for various advanced 
+                    front-end development techniques. The app also uses data from rest apis 
+                    and uses Google's Firestore to store user details. 
+                </p>
+                <br/>
+                <p>
+                    <strong>Key Features:</strong> <br/>
+                   - Context API for Global State : The Context API is employed to manage global state across components, ensuring a centralized and accessible data source.
+                                                    This approach simplifies state management in larger applications with complex component hierarchies.<br/>
+                   - Hooks : React Hooks, such as useState and useEffect, are used to enhance the functional components with stateful logic.
+                             This modern approach allows functional components to manage state and side effects without the need for class components.<br/>
+                   - Optimizing Performance : React's performance optimization strategies, including memoization and PureComponent, are implemented to ensure efficient rendering.
+                                              This results in a responsive user interface with minimized unnecessary re-renders.<br/>
+                   - Conditional Rendering : React's conditional rendering capabilities are employed to display different views based on the application's state.
+                                             This ensures that users are presented with relevant information and features depending on the context of their interactions.
+                </p>
+            </div>
+        </div>
+      </div>
+
+      <div id="project6" className="project-section">
+        <h2>
+        Client Host Media Controller
+        <button className="back-to-top" onClick={scrollToTop}>
+          Back to Top
+        </button>
+        </h2> 
+        <div>
+            <div>
+                <br/>
+                <h3>
+                    <strong>
+                    What the project is about: 
+                    </strong>
+                    </h3>
+                <p>
+                    The Complex Team Management Tool is a sophisticated React application 
+                    designed to streamline and enhance the collaborative workflow within 
+                    complex team structures. Built with a focus on incorporating fundamental
+                    React concepts, this app serves as a showcase for various advanced 
+                    front-end development techniques. The app also uses data from rest apis 
+                    and uses Google's Firestore to store user details. 
+                </p>
+                <br/>
+                <p>
+                    <strong>Key Features:</strong> <br/>
+                   - Context API for Global State : The Context API is employed to manage global state across components, ensuring a centralized and accessible data source.
+                                                    This approach simplifies state management in larger applications with complex component hierarchies.<br/>
+                   - Hooks : React Hooks, such as useState and useEffect, are used to enhance the functional components with stateful logic.
+                             This modern approach allows functional components to manage state and side effects without the need for class components.<br/>
+                   - Optimizing Performance : React's performance optimization strategies, including memoization and PureComponent, are implemented to ensure efficient rendering.
+                                              This results in a responsive user interface with minimized unnecessary re-renders.<br/>
+                   - Conditional Rendering : React's conditional rendering capabilities are employed to display different views based on the application's state.
+                                             This ensures that users are presented with relevant information and features depending on the context of their interactions.
+                </p>
+            </div>
+        </div>
+      </div>
+
+
+
+
     </div>
+
+    
+
+    
   );
 };
 
