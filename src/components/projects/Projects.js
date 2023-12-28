ef9293f9bf82975530e9cf88390d4785b73d6b92
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2023-12-27 19:40:27 -0500

    project component changes.

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,16 +4,25 @@ import React from 'react';
 import Carousel from '../projectslides/Carousel';
 import javaicon from '../../resources/techicon/java.gif'
 import pythonicon from '../../resources/techicon/pythongif.gif'
+import javascripticon from '../../resources/techicon/javascripticon.gif'
 
 import reacticon from '../../resources/skillIcons/reacticon.png'
 import htmlicon from '../../resources/skillIcons/htmlicon.png'
 import cssicon from '../../resources/skillIcons/cssicon.png'
 import booticon from '../../resources/skillIcons/booticon.png'
 import flaskicon from '../../resources/skillIcons/flaskicon.png'
+import phpicon from '../../resources/skillIcons/phpicon.png'
+import firebaseicon from '../../resources/skillIcons/realtimeicon.png'
+import sqlicon from '../../resources/skillIcons/sqlicon.png'
+import giticon from '../../resources/skillIcons/githubicon.png'
 
-import javaimg1 from '../../resources/projectimages/1_jav_img.png'
-import javaimg2 from '../../resources/projectimages/2_jav_img.png'
-import javaimg3 from '../../resources/projectimages/3_jav_img.png'
+
+import javaimg1 from '../../resources/projectimages/1_java/1_java.png'
+import javaimg2 from '../../resources/projectimages/1_java/2_java.png'
+import javaimg3 from '../../resources/projectimages/1_java/3_java.png'
+import javaimg4 from '../../resources/projectimages/1_java/4_java.png'
+import javaimg5 from '../../resources/projectimages/1_java/5_java.png'
+import javaimg6 from '../../resources/projectimages/1_java/6_java.png'
 
 
 
@@ -22,9 +31,12 @@ import javaimg3 from '../../resources/projectimages/3_jav_img.png'
 import './Projects.css'; 
 
 const slides1 = [
+    <img className='project-slides' key={3} src={javaimg3} alt="Slide 3" />,
     <img className='project-slides' key={1} src={javaimg1} alt="Slide 1" />,
     <img className='project-slides' key={2} src={javaimg2} alt="Slide 2" />,
-    <img className='project-slides' key={3} src={javaimg3} alt="Slide 3" />,
+    <img className='project-slides' key={4} src={javaimg4} alt="Slide 4" />,
+    <img className='project-slides' key={5} src={javaimg5} alt="Slide 5" />,
+    <img className='project-slides' key={6} src={javaimg6} alt="Slide 6" />,
   ];
 
 const Projects = () => {
@@ -59,6 +71,7 @@ const Projects = () => {
         <img className="actual-icon" src={htmlicon} />
         <img className="actual-icon" src={cssicon} />
         <img className="actual-icon" src={booticon} />
+        <img className="actual-icon" src={firebaseicon} />
           </div>
         
 
@@ -75,15 +88,44 @@ const Projects = () => {
        
         </div>
         <div className="project-name" onClick={() => scrollToProject('project2')}>
-        Paper Presentation Conference Portal		
+        Paper Presentation Conference Portal	
+        <div>
+        <img className="actual-icon" src={phpicon} />
+        <img className="actual-icon" src={htmlicon} />
+        <img className="actual-icon" src={cssicon} />
+        <img className="actual-icon" src={booticon} />
+        <img className="actual-icon" src={sqlicon} />
+        </div>
+	
         </div>
         <div className="project-name" onClick={() => scrollToProject('project2')}>
-        CNN based Fake News Detector 		
-        </div>
+        CNN based Fake News Detector 	
+        <div>
+        <img className="actual-icon" src={pythonicon} />
+        <img className="actual-icon" src={htmlicon} />
+        <img className="actual-icon" src={cssicon} />
+        <img className="actual-icon" src={booticon} />
+        </div>	
+        </div> 
         <div className="project-name" onClick={() => scrollToProject('project2')}>
-        Client Host Multimedia Player		
+        Client Host Multimedia Player	
+        <div>
+        <img className="actual-icon" src={pythonicon} />
+        <img className="actual-icon" src={reacticon} />
+        <img className="actual-icon" src={javascripticon} />
+        </div>	
         </div>
-        {/* Add more project names as needed */}
+
+        <div className="project-name" onClick={() => scrollToProject('project2')}>
+        This Site ! 
+        <div>
+        <img className="actual-icon" src={reacticon} />
+        <img className="actual-icon" src={htmlicon} />
+        <img className="actual-icon" src={cssicon} />
+        <img className="actual-icon" src={booticon} />
+        <img className="actual-icon" src={giticon} />
+        </div>	
+        </div>
       </div>
 
       <div id="project1" className="project-section">
@@ -93,24 +135,79 @@ const Projects = () => {
         </button>
         </h2> 
         <div>
-        <Carousel slides={slides1} />
-        <p>Description of Project 1...</p>
+            <Carousel slides={slides1} />
+            <div>
+                <br/>
+                <h3>
+                    <strong>
+                    What the project is about :
+                    </strong>
+                </h3>
+                <p>
+                    An image processing software that implements features like image sharpening,
+                    levels adjustment, color correction and much more, the main focus being 
+                    the software's adherance to MVC design pattern along with several industry 
+                    standards focusing on maximum scalability and adaptability to new features. 
+                </p>
+                <br/>
+                <p>
+                    <strong>Key Features:</strong> <br/>
+                    - The software has been written entirely in core java without using any libraries 
+                    apart from the jdk. <br/>
+                    - The software supports preview functionality to reduce 
+                    operation time by operating on a smaller subset of the image matrix.   <br/> 
+                    - The GUI has been implemented in Swing UI and the communication between GUI and 
+                    controller has been designed in a way where it can be easiliy swaped out with any 
+                    other UI library.  <br/>
+                    - Includes a histogram feature similar to the one in Adobe Photoshop where one can see realtime 
+                    changes in image color channels as different operations are performed on it.     
+                </p>
+            </div>
         </div>
         
       
       </div>
 
       <div id="project2" className="project-section">
-        <h2>Project 2</h2>
+        <h2>
+        React Based Complex Team Management Tool 
         <button className="back-to-top" onClick={scrollToTop}>
           Back to Top
         </button>
-        <div className="project-icons">
+        </h2> 
+        <div>
+            <div>
+                <br/>
+                <h3>
+                    <strong>
+                    What the project is about: 
+                    </strong>
+                    </h3>
+                <p>
+                    The Complex Team Management Tool is a sophisticated React application 
+                    designed to streamline and enhance the collaborative workflow within 
+                    complex team structures. Built with a focus on incorporating fundamental
+                     React concepts, this app serves as a showcase for various advanced 
+                     front-end development techniques.   
+                </p>
+                <br/>
+                <p>
+                    <strong>Key Features:</strong> <br/>
+                   - Context API for Global State : The Context API is employed to manage global state across components, ensuring a centralized and accessible data source.
+                                                    This approach simplifies state management in larger applications with complex component hierarchies.<br/>
+                   - Hooks : React Hooks, such as useState and useEffect, are used to enhance the functional components with stateful logic.
+                             This modern approach allows functional components to manage state and side effects without the need for class components.<br/>
+                   - Optimizing Performance : React's performance optimization strategies, including memoization and PureComponent, are implemented to ensure efficient rendering.
+                                              This results in a responsive user interface with minimized unnecessary re-renders.<br/>
+                   - Conditional Rendering : React's conditional rendering capabilities are employed to display different views based on the application's state.
+                                             This ensures that users are presented with relevant information and features depending on the context of their interactions.
+                </p>
+            </div>
         </div>
-        <p>Description of Project 2...</p>
+        
+      
       </div>
-
-      {/* Add more project sections as needed */}
+     
     </div>
   );
 };
--- src/components/projectslides/Carousel.js
+++ src/components/projectslides/Carousel.js
@@ -1,36 +1,28 @@
 import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 import './Carousel.css'
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Carousel = ({ slides }) => {
 
   return (
-  //   <Swiper
-  //   // install Swiper modules
-  //   modules={[Navigation, Pagination, Scrollbar, A11y]}
-  //   spaceBetween={50}
-  //   slidesPerView={3}
-  //   navigation
-  //   pagination={{ clickable: true }}
-  //   scrollbar={{ draggable: true }}
-  //   onSwiper={(swiper) => console.log(swiper)}
-  //   onSlideChange={() => console.log('slide change')}
-  // >
-  //   {slides.map((slide, index) => (
-  //         <SwiperSlide> {slide}</SwiperSlide>
-  //       ))}
-  // </Swiper>
-
 <Swiper
 effect={'cards'}
 grabCursor={true}
-modules={[EffectCards]}
+modules={[EffectCards,Pagination, Navigation]}
 className="mySwiper"
+loop={true}
+pagination={{
+  clickable: true,
+}}
+navigation={true}
 >
 {slides.map((slide) => (
           <SwiperSlide> {slide}</SwiperSlide>
